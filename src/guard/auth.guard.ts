92c253e6ee14615172c43e9e9f346a77b126d404
SaiVenkat.Chitturi <saivenkat.chitturi@ust.com>
2024-09-08 14:37:40 +0530

    Added Token Validation

--- src/guard/auth.guard.ts
+++ src/guard/auth.guard.ts
@@ -1,22 +1,44 @@
 import { Injectable } from "@angular/core";
 import { CanActivate, Router} from "@angular/router";
+import { AuthService } from "./auth.service";
+import { APIResponse } from "../response/api.response";
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class AuthGuard implements CanActivate {
-    public constructor(private router: Router) {}
+    private static logInStatus: boolean = false
+    public constructor(private router: Router, private service: AuthService) {}
+
+    private authorize(): void {
+        let tokenExists: boolean = localStorage.getItem('token') != null &&
+        localStorage.getItem('tokenExpiry') != null &&
+        new Date(localStorage.getItem('tokenExpiry')!) > new Date()
+
+        if (!tokenExists)
+        {
+            AuthGuard.logInStatus = false
+            return
+        }
+
+        this.service.getValidateToken(localStorage.getItem('token')!)
+        .subscribe({
+            next: (data: APIResponse) => AuthGuard.logInStatus = true,
+            error: (errorData: any) => {
+                AuthGuard.logInStatus = false
+                alert('UNAUTHORIZED, YOU SHOULD NOT CREATE OR ALTER TOKEN IN ANY WAY!!!')
+                this.router.navigate(['admin/login'])
+            }
+        })
+    }
 
     public canActivate(): boolean {
-        if (AuthGuard.isLoggedIn())
-            return true
-        return false
+        this.authorize()
+        return AuthGuard.isLoggedIn()
     }
 
     public static isLoggedIn(): boolean {
-        return localStorage.getItem('token') != null &&
-            localStorage.getItem('tokenExpiry') != null &&
-            new Date(localStorage.getItem('tokenExpiry')!) > new Date()
+        return AuthGuard.logInStatus
     }
 }
---
+++ src/guard/auth.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { APIResponse } from '../response/api.response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private baseString: string = 'http://localhost:5000'
+  public constructor(private httpClient: HttpClient) { }
+
+  public getValidateToken(token: string): Observable<APIResponse> {
+    return this.httpClient.get<APIResponse>(`${this.baseString}/validate`,
+    {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+  }
+}
