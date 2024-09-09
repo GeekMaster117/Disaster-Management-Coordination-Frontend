ccccbb140b5fb64f665a7e36f77283a587b70b6f
Rahul.Pashikanti <rahul.pashikanti@ust.com>
2024-09-09 10:55:49 +0530

    Added Navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { provideHttpClient } from '@angular/common/http';
-
+import { MatIconModule } from '@angular/material/icon';
 import { AdminLoginComponent } from '../admin-login/admin-login.component';
 import { AdminRegisterComponent } from '../admin-register/admin-register.component';
 import { AdminHomeComponent } from '../admin-home/admin-home.component';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminLoginComponent,
     AdminRegisterComponent,
-    AdminHomeComponent
+    AdminHomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    MatIconModule
   ],
   providers: [
     provideHttpClient()
